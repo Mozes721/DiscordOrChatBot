1ea78d28a486d379976411584e1aabb3d226e5f6
r.taujenis <r.taujenis@samsung.com>
2022-07-29 14:32:29 +0200

    imported tweepy, generated access token

---
+++ main.py
@@ -0,0 +1,11 @@
+import tweepy
+import time
+
+auth = tweepy.OAuth1UserHandler('y0bdWj5JjGUvFqjP9kaYJ8biF', '0HzXorh6ycHXVMSf2TQGKTwAAGR26QCVj6NWxdDbu7jstBKyC7')
+
+auth.set_access_token('1552932201402191872-uOJrC5ov7eKcaxoUYjvGwcapVl76zF', 'txDDfjApo8OaAThTqT1DbL6VKAIH9CvJEaNBw181y4Lml')
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+user = api.get_profile_banner()
+
