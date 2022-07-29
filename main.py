9d972800f937fddecb40b942a961ef926dbeb829
Ricards Taujenis <MozesThegreat@yahoo.com>
2022-07-29 14:33:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tweepy
 import time
-
+ 
 auth = tweepy.OAuth1UserHandler('y0bdWj5JjGUvFqjP9kaYJ8biF', '0HzXorh6ycHXVMSf2TQGKTwAAGR26QCVj6NWxdDbu7jstBKyC7')
 
 auth.set_access_token('1552932201402191872-uOJrC5ov7eKcaxoUYjvGwcapVl76zF', 'txDDfjApo8OaAThTqT1DbL6VKAIH9CvJEaNBw181y4Lml')
