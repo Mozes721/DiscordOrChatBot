9ff75d7b17ea97ed878580458679ac3d4d1f1c05
Richard Taujenis <Mozesthegreat@yahoo.com>
2022-09-21 00:16:05 +0200

    Connection established with TwitterAPI instance

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
+from dataclasses import dataclass
 from http import client
 from pydoc import cli
 from scripts.twitter_response import TwitterResponseAPI
-from config.config_files import APIkeys
+# from config.config_files import APIkeys
 import tweepy
 import time
 
 def connect():
-    client = tweepy.Client(APIkeys.BearerToken, APIkeys.APIKey, APIkeys.APIKeySecret, APIkeys.AccessToken, APIkeys.AccessTokenSecret)
-    auth = tweepy.OAuth1UserHandler(APIkeys.APIKey, APIkeys.APIKeySecret, APIkeys.AccessToken, APIkeys.AccessTokenSecret)
+    client = tweepy.Client(BearerToken, APIKey, APIKeySecret, AccessToken, AccessTokenSecret)
+    auth = tweepy.OAuth1UserHandler(APIKey, APIKeySecret, AccessToken, AccessTokenSecret)
     api = tweepy.API(auth)
     client_id = client.get_me().data.id
-    return client_id
+    return client, client_id
 
-    tweet = TwitterResponseAPI(twitterUser='Charlie')
+    # tweet = TwitterResponseAPI(twitterUser='Charlie')
 
-    print(tweet.say_hello())
+    # print(tweet.say_hello())
 
-def check_tweets(response):
-    for tweet in response.data:
-                try:
-                    print(tweet.text)
-                    client.create_tweet(in_reply_to_tweet_id=tweet.id, text='hello')
-                    start_id = tweet.id
-                except:
-                    pass
+
+class TwitterAPI:
+    def __init__(self, client, client_id):
+        self.start_id = 1
+        self.client = client 
+        self.response = client.get_users_mentions(client_id, since_id=self.start_id)
+
+    def __post_init__(self):
+        print("Connection has been established..")
+        self.check_tweets()
+
+
+    def check_tweets(self):
+        while True:
+            if self.response.data != None:
+                for tweet in self.response:
+                    try:
+                        print(tweet.text)
+                        self.client.create_tweet(in_reply_to_tweet_id=tweet.id, text='hello')
+                        self.start_id = tweet.id
+                    except Exception as error:
+                        print(error)
+                        
+            time.sleep(5)
 
 if __name__ == '__main__':
-    start_id = 1
-    twitter_bot = connect()
-    while True:
-        response = twitter_bot.get_users_mentions(twitter_bot, since_id=start_id)
-        if response.data != None:
-            check_tweets(response)
-        time.sleep(5)
+    client, client_id = connect()
+    TwitterAPI(client, client_id)
+
+   
 
     
