6d5ffe1fa9f3d5d08ad8f6437333d670b6ed9c29
Richard Taujenis <Mozesthegreat@yahoo.com>
2022-09-24 18:51:06 +0200

    getting weather data(celsius/fahrenheit) from weather API added city_list.json as API accepts IDs to get city data

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from http import client
 import os
 from scripts.twitter_response import TwitterResponseAPI
 from config.config_files import APIkeys
+from api.weather import get_weather_data, get_json_data
 import tweepy
 import time
 
 
 def connect():
-    client = tweepy.Client(APIkeys.BearerToken, APIkeys.APIKey, APIkeys.APIKeySecret, APIkeys.AccessToken, APIkeys.AccessTokenSecret)
-    auth = tweepy.OAuth1UserHandler(APIkeys.APIKey, APIkeys.APIKeySecret, APIkeys.AccessToken, APIkeys.AccessTokenSecret)
+    client = tweepy.Client(APIkeys.BearerToken, APIkeys.APIKey, APIkeys.APIKeySecret, APIkeys.AccessToken,
+                           APIkeys.AccessTokenSecret)
+    auth = tweepy.OAuth1UserHandler(APIkeys.APIKey, APIkeys.APIKeySecret, APIkeys.AccessToken,
+                                    APIkeys.AccessTokenSecret)
     api = tweepy.API(auth)
     client_id = client.get_me().data.id
     return client, client_id
@@ -17,19 +20,19 @@ def connect():
     # tweet = TwitterResponseAPI(twitterUser='Charlie')
 
     # print(tweet.say_hello())
+
+
 @dataclass
 class TwitterAPI:
     client: list
     client_id: str
     start_id: int
     response: str
-        
 
     def __post_init__(self):
         print("Connection has been established..")
         print("%s this is a client", self.client)
         self.check_tweets()
-
 
     def check_tweets(self):
         while True:
@@ -41,16 +44,16 @@ class TwitterAPI:
                         self.start_id = tweet.id
                     except Exception as error:
                         print(error)
-                        
+
             time.sleep(5)
+
 
 if __name__ == '__main__':
     client, client_id = connect()
     start_id = 1
-    while True:
-        response = client.get_users_mentions(client_id, since_id=start_id)
-        TwitterAPI(client, client_id, start_id, response)
+    temp_celsius, temp_fahrenheit = get_weather_data('New York')
+    print(temp_fahrenheit, temp_celsius)
 
-   
-
-    
+    # while True:
+    #     response = client.get_users_mentions(client_id, since_id=start_id)
+    #     TwitterAPI(client, client_id, start_id, response)
