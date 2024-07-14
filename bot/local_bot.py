e18527a44905d46479fc2381beff0be90d19b01c
Mozes721 <MozesThegreat@yahoo.com>
2024-07-14 23:11:02 +0200

    Update local_bot.py

--- bot/local_bot.py
+++ bot/local_bot.py
@@ -19,7 +19,7 @@ class LocalBot:
                 "Hi, human! Is there anything I can help you with?",
                 "Do you need help?",
             ]
-        )
+        ) 
         for word in response.split():
             yield word + " "
             time.sleep(0.05)
